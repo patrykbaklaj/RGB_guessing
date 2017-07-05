881c515c028f4503029401c86bfbf9f2d5bf0efb
Patryk Bakłaj <patrykbaklaj@gmail.com>
2017-07-05 19:31:01 +0200

    Hard and easy mode implemented

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
-var colors = generateRandomColors(6);
 
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var pickedColor = pickColor();
 var squares = document.querySelectorAll(".square");
-
 var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
-
 var messageDisplay = document.querySelector("#message");
-
 var h1 = document.querySelector("h1");
-
 var resetButton = document.getElementById("reset");
+var easyBtn = document.getElementById("easyBtn");
+var hardBtn = document.getElementById("hardBtn");
+
+easyBtn.addEventListener("click", function() {
+  this.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+
+hardBtn.addEventListener("click", function() {
+  this.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+      squares[i].style.backgroundColor = colors[i];
+      squares[i].style.display = "block";
+  }
+});
 
 resetButton.addEventListener("click", function() {
   // generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   // pick random colors from array
   pickedColor = pickColor();
   // change color displacy to match picked color
